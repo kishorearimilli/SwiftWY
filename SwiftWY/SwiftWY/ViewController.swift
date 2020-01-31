592b98f6d4ba5b540a470498ea854884ac2ea7ae
Venkata Arimilli <venkata.arimilli1@t-mobile.com>
2020-01-31 16:03:49 +0530

    Created SwiftWY in xcode with required files.

---
+++ SwiftWY/SwiftWY/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftWY
+//
+//  Created by Venkata Rama Kishore on 31/01/20.
+//  Copyright © 2020 Venkata Rama Kishore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
